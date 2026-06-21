ab9e4f3e31747ee6915408a58272e08385852088
Blessednv <69933661+Blessednv@users.noreply.github.com>
2026-06-21 13:01:32 -0400

    Add initial scaffolds for ByteBites classes

---
+++ models.py
@@ -0,0 +1,35 @@
+class Customer:
+    def __init__(self, name):
+        self.name = name
+        self.purchase_history = []
+
+    def add_to_history(self, order):
+        self.purchase_history.append(order)
+    
+
+class MenuItem:
+    def __init__(self, name, price, category, popularity_rating):
+        self.name = name
+        self.price = price
+        self.category = category
+        self.popularity_rating = popularity_rating
+
+class Menu:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, menu_item):
+        self.items.append(menu_item)
+
+    def filter_by_category(self, category):
+        return [item for item in self.items if item.category == category]
+
+class Order:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, menu_item):
+        self.items.append(menu_item)
+
+    def compute_total(self):
+        return sum(item.price for item in self.items)
